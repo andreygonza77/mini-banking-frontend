fc7a2d90b345dbbe9366de43f681d98e31edd69a
andreygonza77 <gonzalesandrey748@gmail.com>
2026-05-12 18:01:42 +0200

    css added to navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -8,5 +8,9 @@ import { RouterLink } from "@angular/router";
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
